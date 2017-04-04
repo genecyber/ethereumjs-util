a8c6b4cfef1837c3c5c81a51cecfea7ba13c1669
Shannon Code <genecyber@gmail.com>
2017-04-04 15:53:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //const createKeccakHash = require('keccak')
-const secp256k1 = require('secp256k1')
+//const secp256k1 = require('secp256k1')
 const assert = require('assert')
 const rlp = require('rlp')
 const BN = require('bn.js')
